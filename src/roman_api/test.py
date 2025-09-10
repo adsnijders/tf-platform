3a335087433423c4e59da1fd7b1fbdd17fe98bbc
adsnijders <adsnijders@outlook.com>
2025-09-10 14:27:47 +0200

    Added test

--- src/roman_api/test.py
+++
@@ -1,3 +0,0 @@
-from roman_converter.cli import rom_to_ar_conv
-
-print(rom_to_ar_conv("V"))
---
+++ src/roman_api/test_main.py
@@ -0,0 +1,4 @@
+from roman_converter.cli import rom_to_ar_conv
+
+def test_rom_to_ar_conv():
+    assert rom_to_ar_conv("V") == 5
