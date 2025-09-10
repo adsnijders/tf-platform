49e2a370c6abd6bedd9d814b397ed5d1698ca893
adsnijders <adsnijders@outlook.com>
2025-09-10 16:47:27 +0200

    ci: added terraform apply

--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "project_id" {
-    type = string
+    type    = string
     default = "ae-terraform-2025"
 }
 
 variable "region" { 
-    type = string
+    type    = string
     default = "europe-west4" 
 }
 
 variable "artifact_repo_id" {
-    type = string
+    type    = string
     default = "ae-2025-registry"
 }
 
 variable "image_name" {
-    type = string
+    type    = string
     default = "ad-image"
 }
 
 variable "db_username" {
-    type = string
+    type    = string
     default = "fastapi_db"
 }
 
 variable "db_password" {
-    type = string
-    sensitive = true
+    type        = string
+    sensitive   = true
 }
